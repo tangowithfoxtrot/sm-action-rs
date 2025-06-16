5fdd41244027e5e514966c9ab73cfd3d25c64ef9
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2025-06-16 05:59:50 -0700

    Revert "fix: windows, as usual"
    This reverts commit 874475b913a9b4bc4e201b98a0ca0bb4570187b6.
    It didn't work.

---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+################################################################
+# a shim to select the correct binary to execute the sm-action #
+################################################################
+
+arch() {
+  output="$(node -p process.arch)"
+  if [ "$output" = "x64" ]; then
+    ARCH="x86_64"
+  elif [ "$output" = "arm64" ]; then
+    ARCH="aarch64"
+  else
+    error "Unsupported architecture: $output"
+  fi
+
+  echo "$ARCH"
+}
+
+os() {
+  output="$(node -p process.platform)"
+  if [ "$output" = "linux" ]; then
+    PLATFORM="unknown-linux-musl"
+  elif [ "$output" = "darwin" ]; then
+    PLATFORM="apple-darwin"
+  elif [ "$output" = "win32" ]; then
+    PLATFORM="pc-windows-msvc"
+  else
+    error "Unsupported platform: $output"
+  fi
+
+  echo "$PLATFORM"
+}
+
+# Main execution
+main() {
+  echo "Setting up bitwarden/sm-action"
+
+  target_triple="$(arch)-$(os)"
+  "./dist/$target_triple/sm-action"
+}
+
+# Run the script
+main
